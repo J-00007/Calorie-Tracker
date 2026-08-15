a6db8918436725d9a593ec0ead7020feb09eae52
J-00007 <JaiNov007@gmail.com>
2026-08-15 16:55:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,10 @@ def main():
             img = Image.open(camera_photo)
          # Method 1: Scan Food (Uses Device Camera)
     with tab1:
-       camera_photo = st.camera_input("Take a picture of your meal", key="camera_1")
+       with tab1:
+        camera_photo = st.camera_input("Take a picture of your meal", key="camera_1")
+        if camera_photo is not None:
+            img = Image.open(camera_photo)
         if camera_photo is not None:
             img = Image.open(camera_photo)
             st.image(img, caption="Scanned Meal", use_container_width=True)
